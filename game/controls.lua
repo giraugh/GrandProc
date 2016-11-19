959d3ad0367419dc016ae782238e67cf02e349d7
Sam Nolan <samnolan555@gmail.com>
2016-11-19 16:51:10 +1100

    Allow multiple controls at same time

--- game/controls.lua
+++ game/controls.lua
@@ -8,12 +8,18 @@ function handleControls()
   if love.keyboard.isDown("right") then
     print(car.body:getX())
     car.body:applyTorque(CAR_TURN_SPEED)
-  elseif love.keyboard.isDown("left") then
+  end
+
+  if love.keyboard.isDown("left") then
     car.body:applyTorque(-CAR_TURN_SPEED)
-  elseif love.keyboard.isDown("up") then
+  end
+
+  if love.keyboard.isDown("up") then
     carAngle = car.body:getAngle()
     car.body:applyForce(math.cos(carAngle) * CAR_SPEED, math.sin(carAngle) * CAR_SPEED)
-  elseif love.keyboard.isDown("down") then
+  end
+
+  if love.keyboard.isDown("down") then
     carAngle = car.body:getAngle()
     car.body:applyForce(-math.cos(carAngle) * CAR_REVERSE_SPEED, -math.sin(carAngle) * CAR_REVERSE_SPEED)
   end
