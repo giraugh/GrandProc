456831d003d2c5b437bcd5c005a24dab61346c8b
Ewan Breakey <ebreakey@gmail.com>
2016-11-19 16:17:41 +1100

    Rotating looks correct

--- game/main.lua
+++ game/main.lua
@@ -18,5 +18,5 @@ end
 
 function love.draw()
   carGraphic = love.graphics.newImage("graphics/car_blue.png")
-  love.graphics.draw (carGraphic, car.body:getX() + 16, car.body:getY() + 32, car.body:getAngle() + math.pi/2)
+  love.graphics.draw (carGraphic, car.body:getX(), car.body:getY(), car.body:getAngle() + math.pi/2)
 end
--- game/physics/physicsWorld.lua
+++ game/physics/physicsWorld.lua
@@ -7,7 +7,7 @@ CAR_WEIGHT = 1
 
 function placeCars()
   car = {}
-  car.body = love.physics.newBody(world, 32 / 2, 64 / 2, "dynamic")
+  car.body = love.physics.newBody(world, 32, 16, "dynamic")
   car.shape = love.physics.newRectangleShape(32, 64)
   car.fixture = love.physics.newFixture(car.body, car.shape, CAR_WEIGHT)
   car.body:setAngularDamping(0.1)
