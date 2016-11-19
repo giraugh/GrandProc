a0512c4e39eb897bd158e44505d4c5159384517d
Sam Nolan <samnolan555@gmail.com>
2016-11-19 20:37:54 +1100

    Pull the cars back on the map

--- game/main.lua
+++ game/main.lua
@@ -7,8 +7,8 @@ function love.load()
   createTileWorld()
 
   --Create Players
-  car1 = newCar(100, 100, IMAGE_CAR_BLUE)
-  car2 = newCar(200, 200, IMAGE_CAR_RED)
+  car1 = newCar(10, 10, IMAGE_CAR_BLUE)
+  car2 = newCar(10, 12, IMAGE_CAR_RED)
   car2.controls = {left = "a", right = "d", up = "w", down = "s"}
 end
 
